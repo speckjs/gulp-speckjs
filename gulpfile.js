b185bdd75ed7a7f5b87e8be950dee15a0892b343
Nick Balestra <nick@balestra.ch>
2015-09-15 13:23:09 -0700

    Spin off multiple strams for each file in the glob - gulpfile example

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,20 @@
 var gulp = require('gulp');
 var speck = require('./index.js');
 var rename = require("gulp-rename");
+var foreach = require('gulp-foreach');
 
 gulp.task('speck', function() {
-  return gulp.src('./test/fixtures/base.js')
-    .pipe(speck({
-      testFW: 'tape',
-      logs: true
-    }))
-    .pipe(rename({
-      suffix : '_tapeSpec',
-      dirname: 'fixtures/specs'
+  return gulp.src('./test/fixtures/*.js')
+    .pipe(foreach(function(stream, file){
+      return stream
+        .pipe(speck({
+          testFW: 'tape',
+          logs: true
+        }))
+        .pipe(rename({
+          suffix : '_tapeSpec',
+          dirname: 'fixtures/specs'
+        }));
     }))
     .pipe(gulp.dest('./test'));
 });
