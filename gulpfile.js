3b2ba3c37bad1adf9d00276897caa085c8ac4d5b
Nick Balestra <nick@balestra.ch>
2015-09-15 14:30:04 -0700

    Added test and linting tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,14 @@
 var gulp = require('gulp');
 var speck = require('./index.js');
-var rename = require("gulp-rename");
+var rename = require('gulp-rename');
 var foreach = require('gulp-foreach');
 var del = require('del');
 var tape = require('gulp-tape');
+var eslint = require('gulp-eslint');
 
 gulp.task('speck:tape', function() {
   return gulp.src('./test/fixtures/*.js')
-    .pipe(foreach(function(stream, file){
+    .pipe(foreach(function(stream, file) {
       return stream
         .pipe(speck({
           testFW: 'tape',
@@ -23,7 +24,7 @@ gulp.task('speck:tape', function() {
 
 gulp.task('speck:jasmine', function() {
   return gulp.src('./test/fixtures/*.js')
-    .pipe(foreach(function(stream, file){
+    .pipe(foreach(function(stream, file) {
       return stream
         .pipe(speck({
           testFW: 'jasmine',
@@ -37,14 +38,20 @@ gulp.task('speck:jasmine', function() {
     .pipe(gulp.dest('./test'));
 });
 
+gulp.task('eslint', function () {
+  return gulp.src(['gulpfile.js', 'index.js', 'test/specs/*_test.js'])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failOnError());
+});
+
 gulp.task('speck', ['speck:tape', 'speck:jasmine']);
 
 gulp.task('clean', function() {
   return del('./test/fixtures/specs/*.js');
 });
 
-
-gulp.task('test', ['clean', 'speck'], function(){
+gulp.task('test', ['eslint', 'clean', 'speck'], function() {
   return gulp.src('test/specs/*_test.js')
     .pipe(tape());
 });
